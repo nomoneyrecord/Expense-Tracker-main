4f67ce000aed614fa48d3b949d66c50960d53b95
Gary H <3rdeye.ghj@gmail.com>
2023-03-02 15:58:50 -0800

    Basic build out

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,26 @@ function App() {
   const [count, setCount] = useState(0)
 
   return ( 
-<>
-</>
-
+    <>
+    <h1 className="head">Simple Expense Tracker</h1>
+    <h2 className="add-item">Add a new item:</h2>
+      <label className="type-label">Type:
+        <input type="text" className="currency" />
+      </label>
+      <label className="name-label">Name:  
+        <input type="text" className="expense" />
+      </label>
+      <label className="date-label">Date:
+        <input type="text" className="date" />
+      </label>
+      <label className="amount-label">Amount:  
+        <input type="text" className="amount" />
+      </label>
+      <button className="add-button">
+        Add a new expense
+      </button>
+      <table></table>
+    </>
 )}
 
 export default App
