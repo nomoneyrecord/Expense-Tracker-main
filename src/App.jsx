6b56b2f8deb1083070b9663c7dbded3b99688b85
Gary H <3rdeye.ghj@gmail.com>
2023-03-02 18:58:16 -0800

    Tender menu and date picker added

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,31 @@ function App() {
   const [count, setCount] = useState(0)
 
   return ( 
-    <>
-    <h1 className="head">Simple Expense Tracker</h1>
-    <h2 className="add-item">Add a new item:</h2>
-      <label className="type-label">Type:
-        <select name="tender" className="payment-type">
-          <option value="cash">Cash</option>
-          <option value="card">Card</option>
-          <option value="crypto">Crypto</option>
-          <option value="Other">Other</option>
-        </select>
-      </label>
-      <label className="name-label">Name:  
-        <input type="text" className="expense" placeholder="What did you spend on?"/>
-      </label>
-      <label className="date-label">Date:
-        <input type="date" className="date" />
-      </label>
-      <label className="amount-label">Amount:  
-        <input type="text" className="amount" value="0"/>
-      </label>
-      <button className="add-button">
-        Add a new expense
-      </button>
-      <table></table>
-    </>
+    <form>
+      <h1 className="head">Simple Expense Tracker</h1>
+      <h2 className="add-item">Add a new item:</h2>
+        <label className="type-label">Type:
+          <select name="tender" className="payment-type">
+            <option value="cash">Cash</option>
+            <option value="card">Card</option>
+            <option value="crypto">Crypto</option>
+            <option value="Other">Other</option>
+          </select>
+        </label>
+        <label className="name-label">Name:  
+          <input type="text" className="expense" placeholder="What did you spend on?"/>
+        </label>
+        <label className="date-label">Date:
+          <input type="date" className="date" />
+        </label>
+        <label className="amount-label">Amount:  
+          <input type="text" className="amount" placeholder="How much?"/>
+        </label>
+        <button className="add-button">
+          Add a new expense
+        </button>
+      
+    </form>
 )}
 
 export default App
