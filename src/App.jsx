56ebeefb7320a88b929a89ec1b5e20795255193f
Gary H <3rdeye.ghj@gmail.com>
2023-03-02 16:41:14 -0800

    All input fields and selectors are functional

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,21 @@ function App() {
     <h1 className="head">Simple Expense Tracker</h1>
     <h2 className="add-item">Add a new item:</h2>
       <label className="type-label">Type:
-        <input type="text" className="currency" />
+        <select name="tender" className="payment-type">
+          <option value="cash">Cash</option>
+          <option value="card">Card</option>
+          <option value="crypto">Crypto</option>
+          <option value="Other">Other</option>
+        </select>
       </label>
       <label className="name-label">Name:  
-        <input type="text" className="expense" />
+        <input type="text" className="expense" placeholder="What did you spend on?"/>
       </label>
       <label className="date-label">Date:
-        <input type="text" className="date" />
+        <input type="date" className="date" />
       </label>
       <label className="amount-label">Amount:  
-        <input type="text" className="amount" />
+        <input type="text" className="amount" value="0"/>
       </label>
       <button className="add-button">
         Add a new expense
