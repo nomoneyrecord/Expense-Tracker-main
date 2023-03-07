93041e2dc9b5788115e96561103af761e60a255b
Gary H <3rdeye.ghj@gmail.com>
2023-03-06 19:35:26 -0800

    Added additional styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,49 +6,60 @@ function App() {
   const [count, setCount] = useState(0)
 
   return ( 
-<div className="main-content">
-  <h1 className="head">Simple Expense Tracker</h1>
-  <h2 className="add-item">Add a new item:</h2>
-  <label className="type-label">Type:
-    <select name="tender" className="payment-type">
-      <option value="cash">Cash</option>
-      <option value="card">Card</option>
-      <option value="crypto">Crypto</option>
-      <option value="Other">Other</option>
-    </select>
-  </label>
-  <label className="name-label">Name:
-    <input type="text" className="expense" placeholder="What did you spend on?" />
-  </label>
-  <label className="date-label">Date:
-    <input type="date" className="date" />
-  </label>
-  <label className="amount-label">Amount:
-    <input type="text" className="amount" placeholder="How much?" />
-  </label>
-  <button className="add-button">
-    Add a new expense
-  </button>
-  <table className="main-table">
-    <thead className="table-head">
-      <tr>
-        <th>Type</th>
-        <th colSpan="4">Name</th>
-        <th>Date</th>
-        <th>Amount</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-)}
+ 
+  <div className="main-content">
+    <h1 className="head">Simple Expense Tracker</h1>
+    <h2 className="add-item">Add a new item:</h2>
+    <form className="input-form">
+    <div className="row1">
+    <label className="type-label">Type:
+      <select name="tender" className="payment-type">
+        <option value="cash">Cash</option>
+        <option value="card">Card</option>
+        <option value="crypto">Crypto</option>
+        <option value="Other">Other</option>
+      </select>
+    </label>
+    <label className="name-label">Name:     
+      <input type="text" className="expense" placeholder="What was it?" />
+    </label>
+    </div>
+    <div className="row2">
+    <label className="date-label">Date:
+      <input type="date" className="date" />
+    </label>
+    <label className="amount-label">Amount:
+      <input type="text" className="amount" placeholder="How much?" />
+    </label>
+    </div>
+    <div className="button">
+    <button className="add-button">
+      Add a new expense
+    </button>
+    </div>
+    </form>
+    <div className="main-table">
+      <table>
+        <thead className="table-head">
+          <tr>
+            <th>Type</th>
+            <th colSpan="4">Name</th>
+            <th>Date</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  )}
 
 
 
